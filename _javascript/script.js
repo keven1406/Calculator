7b71a0784f47673e255b3c04ad1caf90da7cd97a
Elias Keven <eliaskeven2@gmail.com>
2017-08-13 00:25:47 -0300

    Adding keyboard scripts

--- _javascript/script.js
+++ _javascript/script.js
@@ -1,6 +1,15 @@
-var numberUsed = [], sign;
+var numberUsed = [], sign, algorismOne = "", algorismTwo = "";
 function calculateNumber(number) {
 	numberUsed.push(number);
 	if (numberUsed.length > 2)
 		numberUsed.pop();
+}
+//Using a variable to pass the value of the number.
+
+function addingNumber(number) {
+	if (algorismOne == "")
+		algorismOne = String(number);
+	else
+		algorismOne += number;
+	document.getElementById("value").innerHTML = algorismOne;
 }
