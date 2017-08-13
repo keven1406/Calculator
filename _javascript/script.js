d46b6dc1cb763da880deed8352db918ac36e4a38
Elias Keven <eliaskeven2@gmail.com>
2017-08-13 13:54:49 -0300

    adicionando array e estrutura de dados

--- _javascript/script.js
+++ _javascript/script.js
@@ -1,15 +1,24 @@
-var numberUsed = [], sign, algorismOne = "", algorismTwo = "";
-function calculateNumber(number) {
+var numberUsed = ["",""], sign = "", numberAmount = 0;
+/*function calculateNumber(number) {
 	numberUsed.push(number);
 	if (numberUsed.length > 2)
 		numberUsed.pop();
 }
-//Using a variable to pass the value of the number.
-
-function addingNumber(number) {
-	if (algorismOne == "")
-		algorismOne = String(number);
+*/
+//Using a function showOnScreen to pass the value of the numberUsed to display on the screen.
+function  showOnScreen(algorism, number) {
+	if (algorism == "")
+		algorism = String(number);
 	else
-		algorismOne += number;
-	document.getElementById("value").innerHTML = algorismOne;
+		algorism += String(number);
+	document.getElementById("value").innerHTML = algorism;
+	return algorism;
 }
+function addingNumber(number) {
+	if (sign == "")
+		numberUsed[numberAmount] = showOnScreen(numberUsed[numberAmount], number);
+	else 
+		numberUsed[numberAmount/*2*/] = showOnScreen(numberUsed[numberAmount], number);
+}
+//Using a sign to pass the value of the number for a function and calculate.
+//how to improve this code?
